11daeb8d665dcfadce96031c6fcaa268cea4e847
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-12-07 14:51:47 +0530

    Project-4

---
+++ ConAppProject4/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public char Section { get; set; }
+
+        public Student(string name, int claas, char section)
+        {
+            Name = name;
+            Class = claas;
+            Section = section;
+        }
+    }
+}
